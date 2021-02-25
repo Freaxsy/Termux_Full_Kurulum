62d8b2ed1c9202290294e1c427916e53a27ea123
makrokobra2020 <71518609+makrokobra2020@users.noreply.github.com>
2021-02-25 18:44:35 +0300

    Add files via upload

---
+++ Termux_Full_Kurulum.py
@@ -0,0 +1,47 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+os.system("pkg  update -y")
+os.system("pkg  upgrade -y")
+os.system("pkg install python3 -y")
+os.system("pkg install python2 -y")
+os.system("pkg  install php -y")
+os.system("pkg  install bash -y")
+os.system("pkg  install git -y")
+os.system("pkg  install ninja -y")
+os.system("pkg install apt -y")
+os.system("apt install busybox -y")
+os.system("apt install cmatrix -y")
+os.system("apt install dnsutils -y")
+os.system("apt install hashdeep -y")
+os.system("apt install hexcurse -y")
+os.system("apt install hydra -y")
+os.system("apt install json-c -y")
+os.system("apt install netcat -y")
+os.system("apt install openssl -y")
+os.system("apt install sslscan -y")
+os.system("apt install weechat -y")
+os.system("apt install unzip -y")
+os.system("apt install nmap -y")
+os.system("apt install termux-tools -y")
+os.system("pkg install curl -y")
+os.system("pkg install wget -y")
+os.system("pkg install perl -y")
+os.system("pkg install pip -y")
+os.system("pkg install pip2 -y")
+os.system("pip install wordlist -y")
+os.system("pkg install nmap -y")
+os.system("pkg install hydra -y")
+os.system("pkg install openssl -y")
+os.system("apt install nodejs -y")
+os.system("pkg install sqlmap -y")
+
+print("""
+
+kurulumlar bitti ekranı temizlemek için clear yazın
+Güle GÜle 
+
+""")
+
