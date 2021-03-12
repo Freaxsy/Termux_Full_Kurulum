b90c5f95636c4a46d62b21cd03cc2f7b3aae8c5e
M4kr0 <71518609+M49R0@users.noreply.github.com>
2021-03-12 12:28:10 +0300

    Update Termux_Full_Kurulum.py

--- Termux_Full_Kurulum.py
+++ Termux_Full_Kurulum.py
@@ -2,11 +2,30 @@
 # -*- coding: utf-8 -*-
  
 
-tamam = raw_input("""Kurulumlar Telefonunuzun Gücüne Göre Az veya Uzun Sürebilir. Devam Etmek İstiyormusun y/n: """)
+
 
 import os
 
-if(tamam == "y"):
+os.system("apt install figlet")
+os.system("clear")
+os.system("figlet MAKRO")
+
+print("""
+
+
+1)Kısa ana araçlar
+
+2)Uzun bütün araçlar(Cihazınızın gelişmiş olması lazım
+en az 4 GB boş hafıza 3 GB ram ve 20 mbps internet gerekir
+yoksa sonuçları deneyip görebilirsiniz :D)
+
+3)Çıkış
+
+""")
+
+secim = raw_input("Seçim Yapınız: ")
+
+if(secim =="1"):
 	os.system("pkg install figlet -y")
 	os.system("apt install figlet -y")
 	os.system("apt install ruby -y")
@@ -20,8 +39,40 @@ if(tamam == "y"):
 	os.system("pkg  install git -y")
 	os.system("pkg  install ninja -y")
 	os.system("pkg install apt -y")
+	os.system("apt install hydra -y")
+	os.system("apt install unzip -y")
+	os.system("apt install nmap -y")
+	os.system("apt install termux-tools -y")
+	os.system("pkg install curl -y")
+	os.system("pkg install wget -y")
+	os.system("pkg install pip -y")
+	os.system("pkg install pip2 -y")
+	os.system("pip install wordlist -y")
+	os.system("pkg install nmap -y")
+	os.system("pkg install hydra -y")
+	os.system("pkg install sqlmap -y")	
+	os.system("pkg install openssl -y")
+	os.system("apt install nodejs -y")
+
+elif(secim == "2"):
+	os.system("pkg install figlet -y")
+	os.system("apt install figlet -y")
+	os.system("apt install ruby -y")
+	os.system("clear")
+	os.system(" figlet M4kr0")
+	os.system("pkg  update -y")
+	os.system("pkg  upgrade -y")
+	os.system("apt update -y")
+	os.system("pkg install python3 -y")
+	os.system("pkg install python2 -y")
+	os.system("pkg  install git -y")
+	os.system("pkg  install ninja -y")
+	os.system("pkg install apt -y")
+	os.system("apt upgrade -y")
 	os.system("apt install busybox -y")
+	os.system("apt install nano")
 	os.system("apt install cmatrix -y")
+	os.system("apt install tor -y")
 	os.system("apt install dnsutils -y")
 	os.system("apt install hashdeep -y")
 	os.system("apt install hexcurse -y")
@@ -36,7 +87,6 @@ if(tamam == "y"):
 	os.system("apt install termux-tools -y")
 	os.system("pkg install curl -y")
 	os.system("pkg install wget -y")
-	os.system("pkg install perl -y")
 	os.system("pkg install pip -y")
 	os.system("pkg install pip2 -y")
 	os.system("pip install wordlist -y")
@@ -45,9 +95,13 @@ if(tamam == "y"):
 	os.system("pkg install openssl -y")
 	os.system("apt install nodejs -y")
 	os.system("pkg install sqlmap -y")
+	os.system("pkg install perl -y")
 	
-elif(tamam == "n"):
-	print("Güle Güle")
+elif(secim =="3"):
+	print("""
+
+Güle Güle""")
+
 
 else:
 	print("Yanlış seçim")
@@ -59,5 +113,6 @@ if(tem == "y"):
 	os.system("clear")
 
 elif(tem == "n"):
-	print("Güle Güle")
+	print("""
 
+Güle Güle""")
